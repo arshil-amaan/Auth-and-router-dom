13fe2536d3a78ced61f20b111c1ac45477b7dff4
Arshil <arshilamaanansari@gmail.com>
2024-02-14 14:37:24 +0530

    first commit

---
+++ src/pages/Root.js
@@ -0,0 +1,36 @@
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  // const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration)
+
+    setTimeout(() => {
+      submit(null, {action:'/logout',method:'POST'})
+    }, tokenDuration);
+  }, [token, submit])
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
